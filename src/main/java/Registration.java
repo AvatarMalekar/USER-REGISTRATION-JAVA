03442b06886c3ea79fc275194e214eaf40677700
Your Name <avatarmalekar4@gmail.com>
2020-02-17 19:19:45 +0530

    ADDED:validation for first name

--- src/main/java/Registration.java
+++ src/main/java/Registration.java
@@ -1,2 +1,6 @@
 public class Registration {
+    String NAME_CHECKER="^[A-Z]{1}[a-z]{2,}$";
+    public boolean firstName(String fname) {
+        return fname.matches(NAME_CHECKER);
+    }
 }
--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -1,2 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class RegistrationTest {
+    Registration registration=new Registration();
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+        boolean validFirstNameTrue = registration.firstName("Avatar");
+        Assert.assertEquals(true,validFirstNameTrue);
+    }
+
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturnFalse() {
+        boolean validFirstNameFalse=registration.firstName("abcd");
+        Assert.assertEquals(false,validFirstNameFalse);
+    }
 }
